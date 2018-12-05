bc29ba855866506d92aaff017c6f3d45e9a03c87
serlinecai4935 <scai4935@bths.edu>
2018-12-05 14:40:05 -0500

    1

--- src/Rooms/FunRoom.java
+++ src/Rooms/FunRoom.java
@@ -2,5 +2,25 @@ package Rooms;
 import Game.Runner;
 import People.Person;
 
-public class FunRoom {
+public class FunRoom extends Room {
+    public FunRoom (int x, int y){
+        super (x, y);
+    }
+    @Override
+    public void enterRoom(Person x){
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You found the fun room! Add bonus points.");
+
+        /**
+         * Removes the player from the room.
+         * @param
+         */
+
+        public void leaveRoom(Person x)
+        {
+            occupant = null;
+        }
+    }
 }
